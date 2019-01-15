2a4b90415c540158b40195b6ec56001871756ee7
shanejonas <jonas.shane@gmail.com>
2019-01-14 22:01:05 -0800

    feature: use material grid

--- src/containers/Dashboard.tsx
+++ src/containers/Dashboard.tsx
@@ -8,6 +8,8 @@ import Typography from '@material-ui/core/Typography';
 import { hashesToGH } from '../components/formatters';
 import { VictoryChart, VictoryBar, VictoryLabel } from 'victory';
 import { weiToGwei } from '../components/formatters';
+import Grid from '@material-ui/core/Grid';
+
 
 const config = {
   blockTime: 15, // seconds
@@ -49,72 +51,84 @@ export default (props: any) => {
     <EthRpc method="eth.getBlock" params={['latest', true]}>
       {block => (
         <div>
-          <div style={{display: 'flex', width: '100%' }}>
-            <Card>
-              <CardContent>
-                <div>
-                  <Typography variant="headline">
-                    Block Height
-                  </Typography>
-                  <Typography>{block.number}</Typography>
-                </div>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardContent>
-                <Typography variant="headline">
-                  Gas Price
-                </Typography>
-                <EthRpc method="eth.gasPrice">
-                  {gasPrice => <Typography>{weiToGwei(gasPrice.toNumber())} Gwei</Typography>}
-                </EthRpc>
-              </CardContent>
-            </Card>
+          <Grid container spacing={24}>
+            <Grid item xs={12}>
+              <div style={{display: 'flex'}}>
+                <Card>
+                  <CardContent>
+                    <div>
+                      <Typography variant="headline">
+                        Block Height
+                      </Typography>
+                      <Typography>{block.number}</Typography>
+                    </div>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardContent>
+                    <Typography variant="headline">
+                      Gas Price
+                    </Typography>
+                    <EthRpc method="eth.gasPrice">
+                      {gasPrice => <Typography>{weiToGwei(gasPrice.toNumber())} Gwei</Typography>}
+                    </EthRpc>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardContent>
+                    <div>
+                      <Typography variant="headline">
+                        Network Hash Rate
+                      </Typography>
+                      <HashRate block={block} blockTime={config.blockTime}>
+                        {hashRate => <Typography>{hashRate} GH/s</Typography>}
+                      </HashRate>
+                    </div>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardContent>
+                    <div>
+                      <Typography variant="headline">
+                        Peers
+                      </Typography>
+                      <EthRpc method="net.peerCount">
+                        {peerCount => <Typography>{peerCount}</Typography>}
+                      </EthRpc>
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+            </Grid>
 
-            <Card>
-              <CardContent>
-                <div>
-                  <Typography variant="headline">
-                    Network Hash Rate
-                  </Typography>
-                  <HashRate block={block} blockTime={config.blockTime}>
-                    {hashRate => <Typography>{hashRate} GH/s</Typography>}
-                  </HashRate>
-                </div>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardContent>
-                <div>
-                  <Typography variant="headline">
-                    Peers
-                  </Typography>
-                  <EthRpc method="net.peerCount">
-                    {peerCount => <Typography>{peerCount}</Typography>}
-                  </EthRpc>
-                </div>
-              </CardContent>
-            </Card>
-          </div>
-          <EthRpc method="ext.getBlocks" params={[Math.max(block.number - config.blockHistoryLength + 1, 0), block.number]}>
-            {blocks => {
-               return [
-                 <HashChart height={config.chartHeight} title={`Hash Rate Last ${config.blockHistoryLength} blocks`} data={blocks.map(blockMapHashRate)} />,
-                 <VictoryChart height={config.chartHeight} width={config.chartWidth}>
-                   <VictoryLabel x={25} y={24} text={`Transaction count last ${config.blockHistoryLength} blocks`}/>
-                   <VictoryBar data={blocks.map(blockMapTransactionCount)} />
-                 </VictoryChart>,
-                 <VictoryChart height={config.chartHeight} width={config.chartWidth}>
-                   <VictoryLabel x={25} y={24} text={`Gas Used Last ${config.blockHistoryLength} blocks`}/>
-                   <VictoryBar data={blocks.map(blockMapGasUsed)} />
-                 </VictoryChart>,
-                 <VictoryChart height={config.chartHeight} width={config.chartWidth}>
-                   <VictoryLabel x={25} y={24} text={`Uncles Last ${config.blockHistoryLength} blocks`}/>
-                   <VictoryBar data={blocks.map(blockMapUncles)} />
-                 </VictoryChart>,
-               ]
-            }}
-          </EthRpc>
+            <EthRpc method="ext.getBlocks" params={[Math.max(block.number - config.blockHistoryLength + 1, 0), block.number]}>
+              {blocks => {
+                 return [
+                   <Grid item xs={6}>
+                     <HashChart height={config.chartHeight} title={`Hash Rate Last ${config.blockHistoryLength} blocks`} data={blocks.map(blockMapHashRate)} />,
+                   </Grid>,
+                   <Grid item xs={6}>
+                    <VictoryChart height={config.chartHeight} width={config.chartWidth}>
+                      <VictoryLabel x={25} y={24} text={`Transaction count last ${config.blockHistoryLength} blocks`}/>
+                      <VictoryBar data={blocks.map(blockMapTransactionCount)} />
+                    </VictoryChart>
+                   </Grid>,
+                   <Grid item xs={6}>
+                     <VictoryChart height={config.chartHeight} width={config.chartWidth}>
+                       <VictoryLabel x={25} y={24} text={`Gas Used Last ${config.blockHistoryLength} blocks`}/>
+                       <VictoryBar data={blocks.map(blockMapGasUsed)} />
+                     </VictoryChart>
+                   </Grid>,
+                   <Grid item xs={6}>
+                     <VictoryChart height={config.chartHeight} width={config.chartWidth}>
+                       <VictoryLabel x={25} y={24} text={`Uncles Last ${config.blockHistoryLength} blocks`}/>
+                       <VictoryBar data={blocks.map(blockMapUncles)} />
+                     </VictoryChart>
+                   </Grid>,
+                 ]
+              }}
+            </EthRpc>
+          </Grid>
         </div>
       )}
     </EthRpc>
