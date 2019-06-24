f729790031c80b9ac62bef0705156310f81a9951
shanejonas <jonas.shane@gmail.com>
2019-06-24 08:44:46 -0700

    fix: simplify transaction container

--- src/containers/Transaction.tsx
+++ src/containers/Transaction.tsx
@@ -1,19 +1,13 @@
 import * as React from "react";
-import TxView from "../components/TxView";
-
 import usePromise from "react-use-promise";
+import TxView from "../components/TxView";
 import erpc from "../erpc";
 
 export default function TransactionContainer(props: any) {
-  const { hash } = props.match.params;
-  const [{ transaction, receipt }, error, state] = usePromise(async () => {
-    return {
-      transaction: await erpc.eth_getBlockTransactionCountByHash(hash),
-      receipt: await erpc.eth_getTransactionReceipt(hash),
-    };
-  }, []);
+  const [{ transaction, receipt }, error, state] = usePromise(async () => ({
+    transaction: await erpc.eth_getBlockTransactionCountByHash(props.match.params),
+    receipt: await erpc.eth_getTransactionReceipt(props.match.params),
+  }), []);
 
-  return (
-    <TxView tx={transaction} receipt={receipt} />
-  );
+  return (<TxView tx={transaction} receipt={receipt} />);
 }
