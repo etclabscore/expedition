2070fe18229e3ea20ccd27bd8df4a5d379d0c315
shanejonas <jonas.shane@gmail.com>
2020-11-09 08:08:10 -0800

    fix: handle bigint with maxBy and minBy
    fixes #439

--- src/components/BlockView/BlockGasPrice.tsx
+++ src/components/BlockView/BlockGasPrice.tsx
@@ -1,36 +1,57 @@
 import * as React from "react";
 import { hexToNumber } from "@etclabscore/eserialize";
 import { useTranslation } from "react-i18next";
-import {meanBy, minBy, maxBy} from "lodash";
-
 import { TableCell, TableRow } from "@material-ui/core";
+
+const BigIntMinBy = (list: [], func: (item: any) => BigInt): any => {
+  let min: any;
+  for (const item of list) {
+    if (min === undefined) {
+      min = item;
+      continue;
+    }
+    const r = func(item);
+    if (r < func(min)) {
+      min = item;
+    }
+  }
+  return min;
+};
+
+const BigIntMaxBy = (list: [], func: (item: any) => BigInt): any => {
+  let max: any;
+  for (const item of list) {
+    if (max === undefined) {
+      max = item;
+      continue;
+    }
+    const r = func(item);
+    if (r > func(max)) {
+      max = item;
+    }
+  }
+  return max;
+};
 
 function BlockGasPrice(props: any) {
   const { t } = useTranslation();
-  const {transactions} = props.block;
+  const { transactions } = props.block;
 
-  if (transactions.length === 0) {return <></>;}
+  if (transactions.length === 0) { return null; }
 
   return (
     <>
       <TableRow>
-        <TableCell>{t("Average Gas Price")}</TableCell>
-        <TableCell>
-          { meanBy(transactions, (t: any) => BigInt(t.gasPrice)) }
-        </TableCell>
-      </TableRow>
-
-      <TableRow>
         <TableCell>{t("Min Gas Price")}</TableCell>
         <TableCell>
-          { hexToNumber(minBy(transactions, (t: any) => BigInt(t.gasPrice)).gasPrice) }
+          {hexToNumber(BigIntMinBy(transactions, (tx: any) => BigInt(tx.gasPrice))?.gasPrice || "")}
         </TableCell>
       </TableRow>
 
       <TableRow>
         <TableCell>{t("Max Gas Price")}</TableCell>
         <TableCell>
-          { hexToNumber(maxBy(transactions, (t: any) => BigInt(t.gasPrice)).gasPrice) }
+          {hexToNumber(BigIntMaxBy(transactions, (tx: any) => BigInt(tx.gasPrice))?.gasPrice) || ""}
         </TableCell>
       </TableRow>
     </>
